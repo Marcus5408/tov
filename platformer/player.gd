b882da0e11b6e4f90399aaead2191e398f87965a
Issac Liu <issac@matchatea.dev>
2025-08-19 09:06:00 -0400

    fix: beam tapers incorrectly

--- platformer/player.gd
+++ platformer/player.gd
@@ -10,16 +10,38 @@ const SPAWN_POSITION = Vector2(0, 0)  # Set your spawn position here
 
 func _ready():
     spawn_player()
+    # Add a shader to beam to taper opacity from 0.75 (center) to 0 (edge)
+    var shader := Shader.new()
+    shader.code = '''
+        shader_type canvas_item;
+        void fragment() {
+            vec2 center = vec2(0.5, 0.5);
+            float dist = distance(UV, center) / 0.075;
+            float alpha = mix(0.0, 0.6, dist);
+            COLOR = texture(TEXTURE, UV);
+            COLOR.a *= alpha;
+        }
+    '''
+    var mat := ShaderMaterial.new()
+    mat.shader = shader
+    beam.material = mat
 
 
 func spawn_player():
     self.position = SPAWN_POSITION
     self.velocity = Vector2.ZERO
 
+
 @onready var sprite: Sprite2D = $Sprite2D
 @onready var beam: Sprite2D = $Beam
 var prev_direction: float = 0.0
+var beam_visible := true
 func _physics_process(delta: float) -> void:
+    # Toggle beam visibility with debug_toggle_beam
+    if Input.is_action_just_pressed("debug_toggle_beam"):
+        beam_visible = !beam_visible
+        beam.visible = beam_visible
+
     # Add the gravity.
     if not is_on_floor():
         velocity += get_gravity() * delta
@@ -38,7 +60,10 @@ func _physics_process(delta: float) -> void:
     if direction:
         velocity.x = direction * SPEED
         sprite.flip_h = direction < 0
-        beam.flip_h = direction < 0
+        if beam.rotation == deg_to_rad(0) or beam.rotation == deg_to_rad(180):
+            # Only flip the beam if it's horizontal
+            # This prevents flipping when sensing directions
+            beam.flip_h = direction < 0
     else:
         velocity.x = move_toward(velocity.x, 0, SPEED)
 
@@ -50,6 +75,7 @@ func _physics_process(delta: float) -> void:
 
     move_and_slide()
 
-func _on_void_body_entered(body:Node2D) -> void:
+
+func _on_void_body_entered(body: Node2D) -> void:
     if body == self:
         spawn_player()
