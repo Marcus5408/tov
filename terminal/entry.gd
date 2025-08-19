3ef7d8ede506fcba7464725dc2ddf9bf3d890e33
Issac Liu <issac@matchatea.dev>
2025-08-19 06:50:55 -0400

    fix: terminal did not reset upon sending command

--- terminal/entry.gd
+++ terminal/entry.gd
@@ -1,6 +1,6 @@
 extends HBoxContainer
 
-signal text_entered(prompt: String, response: String)
+signal text_entered(response: String)
 @onready var prompt_label: RichTextLabel = get_first_rich_text_label()
 @onready var line_edit: LineEdit = get_node_or_null("LineEdit")
 
@@ -22,5 +22,4 @@ func _gui_input(event):
             line_edit.grab_focus()
 
 func _on_text_submitted(new_text: String) -> void:
-    var prompt = prompt_label.get_text()
-    emit_signal("text_entered", prompt, new_text)
+    emit_signal("text_entered", ("> " + new_text))
