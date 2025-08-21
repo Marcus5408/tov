3e4179edb53b48b387388684624e5793f83847cf
Issac Liu <issac@matchatea.dev>
2025-08-21 01:28:33 -0400

    refactor: rm debug print

--- platformer/camera.gd
+++ platformer/camera.gd
@@ -5,8 +5,6 @@ extends Camera2D
 
 func _process(_delta: float) -> void:
     background.set_anchors_preset(Control.PRESET_CENTER)
-
     background.global_position = get_screen_center_position() - background.size / 2
+
     player_void.transform.origin.x = get_screen_center_position().x
-    print("Void position:", player_void.position)
-    
