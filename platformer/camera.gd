00be1228d87ed51f5c4f8a5fa214f9dfc4f8e4a2
Issac Liu <issac@matchatea.dev>
2025-08-21 00:43:26 -0400

    fix: void disappears after a while, fix by making void follow camera

--- platformer/camera.gd
+++ platformer/camera.gd
@@ -1,7 +1,12 @@
 extends Camera2D
 
 @onready var background: ColorRect = get_node("ColorRect")
+@onready var player_void: CollisionShape2D = self.get_parent().get_parent().get_node("Void/CollisionShape2D")
 
 func _process(_delta: float) -> void:
     background.set_anchors_preset(Control.PRESET_CENTER)
+
     background.global_position = get_screen_center_position() - background.size / 2
+    player_void.transform.origin.x = get_screen_center_position().x
+    print("Void position:", player_void.position)
+    
